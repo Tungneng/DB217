fac9ecb981ae75c6315554d09d0d5ebc8544fe9d
Tungneng <66112009@dpu.ac.th>
2023-10-19 11:26:33 +0700

    pao ying chup

---
+++ midterm/midterm/app.js
@@ -0,0 +1,22 @@
+let mark = [
+  [0, 0, 0, 0],
+  [0, 0, 0, 0],
+  [0, 0, 0, 0],
+  [0, 0, 0, 0],
+];
+let black = 0;
+
+function summary() {
+  // เพิ่มคำสั่งที่นี่
+}
+
+function flip(pawn) {
+  // เพิ่มคำสั่งที่นี่
+}
+
+function main() {
+  // เพิ่มคำสั่งที่นี่
+  summary();
+}
+
+main();
